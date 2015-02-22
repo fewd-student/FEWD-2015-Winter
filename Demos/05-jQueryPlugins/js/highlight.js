62028ccec0467d015e7a63633c86a7ff642dc061
Ben Plum <mr@benplum.com>
2015-02-22 09:44:18 -0500

    jQuery plugin demos

---
+++ Demos/05-jQueryPlugins/js/highlight.js
@@ -0,0 +1,17 @@
+
+	(function($) {
+
+		function onClick(e) {
+			var $target = $(e.target),
+				$sibs = $target.siblings();
+
+			$sibs.removeClass("highlight");
+
+			$target.addClass("highlight");
+		}
+
+		$.fn.highlight = function() {
+			return $(this).on("click", onClick);
+		};
+
+	})(jQuery);
